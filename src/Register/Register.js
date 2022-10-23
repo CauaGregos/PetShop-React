1583d427d3a36578d2061d2bd2348b417bb1f0c2
livinha19 <liviafrancis2004@gmail.com>
2022-10-22 22:28:42 -0300

    fix2210

---
+++ src/Register/Register.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './style.css'
+import Cat from '../assets/cat.png'
+import Dog from '../assets/dog.png'
+
+function Register() {
+    const login = () => {
+
+    };
+  return (
+    <div className="container">
+        <div className="userView">
+            <img className="dogImage" src={Dog}/>
+            <img className="catImage" src={Cat}/>
+
+            <div className="textView">
+                <text className="description">Aqui você encontra tudo para seu pet!</text> 
+            </div>
+            <div className="subTextView">
+                <text className="subdescription">Nosso dever e fazer bem à aquele que te faz bem?</text> 
+            </div>
+        
+        </div>
+        <div className="loginView">
+            <div style={{marginTop:'30%'}}>
+                <input className="inputEmail" placeholder="Nome Completo"/>
+                <input className="inputEmail" placeholder="Email"/>
+                <input className="inputSenha" placeholder="Senha"/>
+                <button className="buttonSubmit" onClick={e=>login()}>Finalizar meu cadastro</button>
+                <text className="wantCreateAccount" >Já tenho uma conta.<a href="#">Fazer login</a></text>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Register;
--- src/Rotes/Rotas.js
+++ src/Rotes/Rotas.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from '../Login/Login';
-
+import Register from '../Register/Register';
 export default function Rotas() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/"  element={<Login/>} component={Login} />      
+                <Route path="/"  element={<Login/>} component={Login} /> 
+                <Route path="/Register"  element={<Register/>} component={Register} />     
             </Routes>
         </BrowserRouter>
         
